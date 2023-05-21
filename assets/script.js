1da366183219f0ce604f3418fdfea71258093105
Jackspence6 <jackspence123456@gmail.com>
2023-05-21 18:55:34 +0200

    Added win, loss or draw calculator.

--- assets/script.js
+++ assets/script.js
@@ -15,19 +15,29 @@ var userChoice = window.prompt("Please enter R, P or S");
 userChoice = userChoice.toUpperCase();
 
 if (!userChoice) {
-    console.log("No Play");
+  console.log("No Play");
 }
 
 /* --------------------------------------- Computer choice -------------------------------------- */
-var pcIndex = Math.floor(Math.random() * entryOptions.length)
+var pcIndex = Math.floor(Math.random() * entryOptions.length);
 var computerChoice = entryOptions[pcIndex];
 
-window.alert("The computer chose " + computerChoice)
+window.alert("The computer chose " + computerChoice);
 
 /* ----------------------- Ensure that user only selects available options ---------------------- */
 if (!userEntryOptions.includes(userChoice)) {
-    console.log("Please select between R, P, and S only!");
+  console.log("Please select between R, P, and S only!");
 }
 
-
-
+/* -------------------------------- Win, loss or draw calculator -------------------------------- */
+if (computerChoice === userChoice ) {
+   console.log("tie"); 
+} else if (
+    (computerChoice === "R" && userChoice === "S") ||
+    (computerChoice === "P" && userChoice === "R") ||
+    (computerChoice === "S" && userChoice === "P") 
+) {
+   console.log("loss"); 
+} else {
+    console.log("win");
+} 
