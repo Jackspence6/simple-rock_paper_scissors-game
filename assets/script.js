a359ebf8f2446c3edbf96ee40bd2c9fab9aa3f93
Jackspence6 <jackspence123456@gmail.com>
2023-05-21 16:49:31 +0200

    Added computer entry options.

--- assets/script.js
+++ assets/script.js
@@ -2,3 +2,7 @@
 var wins = 0;
 var losses = 0;
 var ties = 0;
+
+/* ------------------------------------- Computer entry options ------------------------------------- */
+var entryOptions = ["R", "P", "S"];
+
