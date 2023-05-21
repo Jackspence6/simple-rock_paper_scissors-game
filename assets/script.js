dc8b77ca94061fecedf09437545856e70dbea97a
Jackspence6 <jackspence123456@gmail.com>
2023-05-21 19:11:27 +0200

    Nested script for game play into playGame function.

--- assets/script.js
+++ assets/script.js
@@ -9,45 +9,49 @@ var entryOptions = ["R", "P", "S"];
 /* ------------------------------------- User entry options ------------------------------------- */
 var userEntryOptions = ["R", "P", "S", "r", "p", "s"];
 
-/* --------------------------------- Users choice to play or not -------------------------------- */
-var userChoice = window.prompt("Please enter R, P or S");
+function playGame() {
+  /* --------------------------------- Users choice to play or not -------------------------------- */
+  var userChoice = window.prompt("Please enter R, P or S");
 
-/* ------------------ Converting userChoice to upper case for easier comparison ----------------- */
-if (userChoice !== null) {
- userChoice = userChoice.toUpperCase();   
-}
+  /* ------------------ Converting userChoice to upper case for easier comparison ----------------- */
+  if (userChoice !== null) {
+    userChoice = userChoice.toUpperCase();
+  }
 
-/* ----------------------- If statement to determine if user plays or not ----------------------- */
-if (!userChoice) {
-  console.log("No Play");
-}
+  /* ----------------------- If statement to determine if user plays or not ----------------------- */
+  if (!userChoice) {
+    console.log("No Play");
+  }
 
-/* --------------------------------------- Computer choice -------------------------------------- */
-var pcIndex = Math.floor(Math.random() * entryOptions.length);
-var computerChoice = entryOptions[pcIndex];
+  /* --------------------------------------- Computer choice -------------------------------------- */
+  var pcIndex = Math.floor(Math.random() * entryOptions.length);
+  var computerChoice = entryOptions[pcIndex];
 
-window.alert("The computer chose " + computerChoice);
+  window.alert("The computer chose " + computerChoice);
 
-/* ----------------------- Ensure that user only selects available options ---------------------- */
-if (!userEntryOptions.includes(userChoice)) {
-  console.log("Please select between R, P, and S only!");
-}
+  /* ----------------------- Ensure that user only selects available options ---------------------- */
+  if (!userEntryOptions.includes(userChoice)) {
+    console.log("Please select between R, P, and S only!");
+  }
 
-/* -------------------------------- Win, loss or draw calculator -------------------------------- */
-if (computerChoice === userChoice ) {
-   console.log("tie"); 
-   ties ++;
-} else if (
+  /* -------------------------------- Win, loss or draw calculator -------------------------------- */
+  if (computerChoice === userChoice) {
+    console.log("tie");
+    ties++;
+  } else if (
     (computerChoice === "R" && userChoice === "S") ||
     (computerChoice === "P" && userChoice === "R") ||
-    (computerChoice === "S" && userChoice === "P") 
-) {
-   console.log("loss"); 
-   losses ++;
-} else {
+    (computerChoice === "S" && userChoice === "P")
+  ) {
+    console.log("loss");
+    losses++;
+  } else {
     console.log("win");
-    wins ++;
-} 
+    wins++;
+  }
 
-/* ---------------------------- Wins, Losses and Ties live calculator --------------------------- */
-console.log("Stats: " + "wins: " + wins + ", Losses: " + losses + ", Ties: " + ties);
+  /* ---------------------------- Wins, Losses and Ties live calculator --------------------------- */
+  console.log(
+    "Stats: " + "wins: " + wins + ", Losses: " + losses + ", Ties: " + ties
+  );
+}
