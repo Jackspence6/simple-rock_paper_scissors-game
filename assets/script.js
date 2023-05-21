a1ea053a129cb79f4eddbad30c4fe498b71ff819
Jackspence6 <jackspence123456@gmail.com>
2023-05-21 17:19:20 +0200

    Added computer choice.

--- assets/script.js
+++ assets/script.js
@@ -8,7 +8,17 @@ var entryOptions = ["R", "P", "S"];
 
 /* --------------------------------- Users choice to play or not -------------------------------- */
 var userChoice = window.prompt("Please enter R, P or S");
-    if (!userChoice) {
-        return;
-    }
+
+if (!userChoice) {
+    console.log("No Play");
+}
+
+/* --------------------------------------- Computer choice -------------------------------------- */
+var pcIndex = Math.floor(Math.random() * entryOptions.length)
+var computerChoice = entryOptions[pcIndex];
+
+window.alert("The computer chose " + computerChoice)
+
+
+
 
